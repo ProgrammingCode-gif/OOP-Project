c739ca5e13c019b9026552b9f91b257a441f2ad7
kamikotoin <166481079+kamikotoin@users.noreply.github.com>
2025-04-23 01:51:05 +0500

    try

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    Grade g1(1, "Calculus", 100, "20.03.25");
+    Grade g1(1, "Calculus", 99, "20.03.25");
     Grade g2(2, "OOP", 100, "21.04.25");
     Grade* grades = new Grade[2]{g1, g2};
     Student s1(243, "Shahzod", "Tashev", grades, 2);
