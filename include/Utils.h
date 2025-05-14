6f2048a14d60c679b5d8495d2a45ae4338e6312a
Shahzod <shahzodtashev49@gmail.com>
2025-05-14 23:21:54 +0500

    .

---
+++ include/Utils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Student.h"
+#include "Group.h"
+#include "Attendence.h"
+#include <string>
+
+void handle_course(Student &student, Attendence *attendenceArray, int subjectId, const std::string &courseName);
+
+void add_grade(Student &stud, int newSubjectId, const std::string &newSubjectName, int newScore, const std::string &newDate);
+
+void add_grade2(Group &group);
+
+void read_grades(std::fstream &file, Grade *grades, int count);
+
+void read_student(std::fstream &file, Student &student, Grade *grades, int gradeCount);
--- main.cpp
+++ main.cpp
@@ -5,68 +5,10 @@
 #include "./include/Attendence.h"
 #include "./include/Student.h"
 #include "./include/Teacher.h"
+#include "./include/Utils.h"
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
-void handle_course(Student &student, Attendence *attendenceArray, int subjectId, const string &courseName)
-{
-    int choosing;
-    cout << "What you want to check for " << courseName << "?\n1)Marks\n2)Attendence\nEnter: ";
-    cin >> choosing;
-
-    switch (choosing)
-    {
-    case 1:
-        student.get_Grades_by_Subject(subjectId);
-        break;
-    case 2:
-        cout << "Date\t\t Attendence\n";
-        for (int i = 0; i < 4; i += 2)
-        {
-            attendenceArray[i].Log_student(0); // Здесь можно сделать гибким выбор студента
-        }
-        break;
-    default:
-        cout << "Wrong!";
-        break;
-    }
-}
-
-void add_grade(Student &stud, int newSubjectId, string newSubjectName, int newScore, string newDate)
-{
-    Grade g(newSubjectId, newSubjectName, newScore, newDate);
-    stud.add_grade(g);
-}
-
-void add_grade2(Group &group)
-{
-    int score, choosing;
-    string date;
-    group.log();
-    cout << "Choose student to add mark:\n";
-    cin >> choosing;
-    cout << "Enter date of the mark:";
-    cin >> date;
-    cout << "Enter score of the mark:";
-    cin >> score;
-    add_grade(group.get_student_ref(choosing - 1), 1, "OOP", score, date);
-    group.get_student_ref(choosing - 1).Log();
-    group.get_student_ref(choosing - 1).Log();
-};
-void read_grades(fstream &file, Grade* grades, int count) {
-    for (int i = 0; i < count; ++i) {
-        Grade g;
-        g.read_data(file);
-        grades[i] = g;
-    }
-}
-void read_student(fstream &file, Student &student, Grade* grades, int gradeCount) {
-    student = Student(grades, gradeCount);
-    student.read_data(file);
-}
-
-
 
 int main()
 {
